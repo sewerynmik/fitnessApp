2b657434a599832735b388de666d36deb526ff1c
Adrian Rojek <kejor007@wp.pl>
2024-11-21 23:02:08 +0100

    working validation while editing profile data

--- app/src/main/java/com/example/aplikacjafitness/Profile.kt
+++ app/src/main/java/com/example/aplikacjafitness/Profile.kt
@@ -5,6 +5,9 @@ import android.content.Intent
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputFilter
+import android.text.InputType
+import android.text.Spanned
 import android.view.LayoutInflater
 import android.widget.EditText
 import android.widget.Toast
@@ -15,8 +18,10 @@ import androidx.compose.material3.AlertDialog
 import androidx.compose.ui.semantics.dismiss
 import androidx.compose.ui.semantics.text
 import androidx.appcompat.app.AlertDialog
+import androidx.compose.ui.semantics.error
 import androidx.compose.ui.semantics.setText
 import androidx.core.content.edit
+import java.util.regex.Pattern
 import kotlin.text.toFloatOrNull
 
 class Profile : AppCompatActivity() {
@@ -64,8 +69,8 @@ class Profile : AppCompatActivity() {
         profEmailTextView.text = sharedPreferences.getString("EMAIL", "")
         profNameTextView.text = sharedPreferences.getString("NAME", "")
         profSurTextView.text = sharedPreferences.getString("SURNAME", "")
-        profWeightTextView.text = sharedPreferences.getFloat("WEIGHT", 23F).toString()
-        profHeightTextView.text = sharedPreferences.getFloat("HEIGHT", 23F).toString()
+        profWeightTextView.text = sharedPreferences.getFloat("WEIGHT", 2F).toString()
+        profHeightTextView.text = sharedPreferences.getFloat("HEIGHT", 2F).toString()
         profStepGoalTextView.text = sharedPreferences.getString("DAILY_STEP_GOAL", "6000")
     }
 
@@ -77,49 +82,91 @@ class Profile : AppCompatActivity() {
         val heightEditText = dialogView.findViewById<EditText>(R.id.heightEditText)
         val stepsEditText = dialogView.findViewById<EditText>(R.id.stepsEditText)
 
+        // Input validation for weightEditText
+        weightEditText.inputType = InputType.TYPE_CLASS_NUMBER or InputType.TYPE_NUMBER_FLAG_DECIMAL
+        weightEditText.filters = arrayOf(InputFilter.LengthFilter(5), WeightInputFilter())
+
+        heightEditText.inputType = InputType.TYPE_CLASS_NUMBER or InputType.TYPE_NUMBER_FLAG_DECIMAL
+        heightEditText.filters = arrayOf(InputFilter.LengthFilter(5), HeightInputFilter())
+
+        // Input validation for nameEditText and surnameEditText
+        nameEditText.filters = arrayOf(InputFilter.LengthFilter(20), NameInputFilter())
+        surnameEditText.filters = arrayOf(InputFilter.LengthFilter(20), NameInputFilter())
+
         nameEditText.setText(sharedPreferences.getString("NAME", ""))
         surnameEditText.setText(sharedPreferences.getString("SURNAME", ""))
         weightEditText.setText(sharedPreferences.getFloat("WEIGHT", 0f).toString())
-        heightEditText.setText(sharedPreferences.getFloat("HEIGHT", 0f).toString())
+        heightEditText.setText(sharedPreferences.getFloat("HEIGHT", 1f).toString())
         val dailyStepGoalString = sharedPreferences.getString("DAILY_STEP_GOAL", "6000")
         stepsEditText.setText(dailyStepGoalString)
+        val activityContext = this
 
         val builder = AlertDialog.Builder(this)
             .setView(dialogView)
             .setTitle("Edit Profile")
             .setPositiveButton("Save") { dialog, _ ->
+                var isValid = true
+                var errorMessage = mutableListOf<String>()
 
-                sharedPreferences.edit {
-                    val name = nameEditText.text.toString()
-                    if (name.isNotEmpty()) {
-                        putString("NAME", name)
-                    }
-
-                    val surname = surnameEditText.text.toString()
-                    if (surname.isNotEmpty()) {
-                        putString("SURNAME", surname)
-                    }
-
-                    val weight = weightEditText.text.toString().toFloatOrNull()
-                    if (weight != null) {
-                        putFloat("WEIGHT", weight)
-                    }
-
-                    val height = heightEditText.text.toString().toFloatOrNull()
-                    if (height != null) {
-                        putFloat("HEIGHT", height)
-                    }
-
-                    val steps = stepsEditText.text.toString().toIntOrNull()
-                    if (steps != null) {
-                        putString("DAILY_STEP_GOAL", steps.toString())
-                    }
-
-                    apply()
+                if (nameEditText.text.toString().length < 3) {
+                    errorMessage.add("Name is too short, it has to be at least 3 letters.\n")
+                    isValid = false
+                } else {
+                    nameEditText.error = null
                 }
 
-                loadData()
-                dialog.dismiss()
+                if (surnameEditText.text.toString().length < 3) {
+                    errorMessage.add( "Surname is too short, it has to be at least 3 letters.\n")
+                    isValid = false
+                } else {
+                    surnameEditText.error = null
+                }
+
+                val height = heightEditText.text.toString().toFloatOrNull()
+                if (height == null || height <= 100 || height > 250) {
+                    errorMessage.add("Please input real height")
+                    isValid = false
+                } else {
+                    heightEditText.error = null
+                }
+
+                if(isValid){
+                    sharedPreferences.edit {
+                        val name = nameEditText.text.toString()
+                        if (name.isNotEmpty()) {
+                            putString("NAME", name)
+                        }
+
+                        val surname = surnameEditText.text.toString()
+                        if (surname.isNotEmpty()) {
+                            putString("SURNAME", surname)
+                        }
+
+                        val weight = weightEditText.text.toString().toFloatOrNull()
+                        if (weight != null) {
+                            putFloat("WEIGHT", weight)
+                        }
+
+                        val height = heightEditText.text.toString().toFloatOrNull()
+                        if (height != null) {
+                            putFloat("HEIGHT", height)
+                        }
+
+                        val steps = stepsEditText.text.toString().toIntOrNull()
+                        if (steps != null) {
+                            putString("DAILY_STEP_GOAL", steps.toString())
+                        }
+
+                        apply()
+                    }
+
+                    loadData()
+                    dialog.dismiss()
+                }
+                if(!isValid){
+                    val errorMessage = errorMessage.joinToString("\n") // Join error messages with newline
+                    Toast.makeText(this@Profile, errorMessage, Toast.LENGTH_SHORT).show()
+                }
             }
             .setNegativeButton("Cancel") { dialog, _ ->
                 dialog.dismiss()
@@ -151,5 +198,40 @@ class Profile : AppCompatActivity() {
         }
         val dialog = builder.create()
         dialog.show()
+    }
+}
+
+class HeightInputFilter : InputFilter {
+    override fun filter(source: CharSequence, start: Int, end: Int, dest: Spanned, dstart: Int, dend: Int): CharSequence? {
+        val input = dest.toString() + source.toString()
+        val pattern = Pattern.compile("^\\d{1,3}(\\.\\d{0,1})?$")
+        return if (pattern.matcher(input).matches()) {
+            null
+        } else {
+            ""
+        }
+    }
+}
+
+class NameInputFilter : InputFilter {
+    override fun filter(source: CharSequence, start: Int, end: Int, dest: Spanned, dstart: Int, dend: Int): CharSequence? {
+        for (i in start until end) {
+            if (!Character.isLetter(source[i]) && source[i] != ' ') {
+                return ""
+            }
+        }
+        return null
+    }
+}
+
+class WeightInputFilter : InputFilter {
+    override fun filter(source: CharSequence, start: Int, end: Int, dest: Spanned, dstart: Int, dend: Int): CharSequence? {
+        val input = dest.toString() + source.toString()
+        val pattern = Pattern.compile("^\\d{1,3}(\\.\\d{0,2})?$")
+        return if (pattern.matcher(input).matches()) {
+            null
+        } else {
+            ""
+        }
     }
 }
